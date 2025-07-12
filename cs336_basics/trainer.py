d7587c0ed3cf3361b65d10a6f2d472bbea97441e
sungdk0822 <sungdk0822@naver.com>
2025-07-13 02:47:40 +0900

    implement cosine_lr_schedule function

--- cs336_basics/trainer.py
+++ cs336_basics/trainer.py
@@ -1,3 +1,4 @@
+import math
 import torch
 from collections.abc import Callable
 from jaxtyping import Float, Int
@@ -76,6 +77,52 @@ class AdamW(torch.optim.Optimizer):
                 state['v'] = v
 
         return loss
+
+
+'''
+The cosine annealing learning rate schedule takes 
+    (i) the current iteration t, 
+    (ii) the maximum learning rate alpha_max, 
+    (iii) the minimum (final) learning rate alpha_min, 
+    (iv) the number of warm-up iterations T_w, and 
+    (v) the number of cosine annealing iterations T_c.
+'''
+'''
+    Given the parameters of a cosine learning rate decay schedule (with linear
+    warmup) and an iteration number, return the learning rate at the given
+    iteration under the specified schedule.
+
+    Args:
+        it (int): Iteration number to get learning rate for.
+        max_learning_rate (float): alpha_max, the maximum learning rate for
+            cosine learning rate schedule (with warmup).
+        min_learning_rate (float): alpha_min, the minimum / final learning rate for
+            the cosine learning rate schedule (with warmup).
+        warmup_iters (int): T_w, the number of iterations to linearly warm-up
+            the learning rate.
+        cosine_cycle_iters (int): T_c, the number of cosine annealing iterations.
+
+    Returns:
+        Learning rate at the given iteration under the specified schedule.
+'''
+def cosine_lr_schedule(
+    it: int,
+    max_learning_rate: float,
+    min_learning_rate: float,
+    warmup_iters: int,
+    cosine_cycle_iters: int,
+):
+    if it < warmup_iters:
+        return it * max_learning_rate / warmup_iters
+    elif warmup_iters <= it <= cosine_cycle_iters:
+        return (
+            min_learning_rate 
+            + 0.5 
+            * (1 + math.cos(math.pi * (it - warmup_iters) / (cosine_cycle_iters - warmup_iters))) 
+            * (max_learning_rate - min_learning_rate)
+        )
+    elif it > cosine_cycle_iters:
+        return min_learning_rate
 
 
 if __name__ == '__main__':
