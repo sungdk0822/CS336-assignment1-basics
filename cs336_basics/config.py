6c7cac1340c3458ee67804fca1ffeb35300f3882
sungdk0822 <sungdk0822@naver.com>
2025-07-16 03:07:00 +0900

    minor change

--- cs336_basics/config.py
+++ cs336_basics/config.py
@@ -3,8 +3,8 @@ import os
 TinyStories_train_set_path = 'cs336_basics/data/TinyStoriesV2-GPT4-train.txt'
 TinyStories_validation_set_path = 'cs336_basics/data/TinyStoriesV2-GPT4-valid.txt'
 
-corpus_path = TinyStories_validation_set_path
-validation_corpus_path = TinyStories_validation_set_path # todo: decouple train corpus and valiation corpus
+corpus_path = TinyStories_train_set_path
+validation_corpus_path = TinyStories_validation_set_path
 
 corpus_name = os.path.splitext(os.path.basename(corpus_path))[0]
 vocab_path = f'cs336_basics/vocab-{corpus_name}.pickle'
