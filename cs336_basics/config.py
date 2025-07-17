513d66852678ec1912369533d288cf55758396a0
sungdk0822 <sungdk0822@naver.com>
2025-07-18 01:27:09 +0900

    minor change

--- cs336_basics/config.py
+++ cs336_basics/config.py
@@ -3,6 +3,7 @@ import os
 TinyStories_train_set_path = 'cs336_basics/data/TinyStoriesV2-GPT4-train.txt'
 TinyStories_validation_set_path = 'cs336_basics/data/TinyStoriesV2-GPT4-valid.txt'
 
+bpe_train_corpus_path = TinyStories_train_set_path
 corpus_path = TinyStories_train_set_path
 validation_corpus_path = TinyStories_validation_set_path
 
@@ -13,4 +14,4 @@ merges_path = f'cs336_basics/merges-{corpus_name}.pickle'
 tokenizer_path = 'cs336_basics/checkpoint/tokenizer.pth'
 corpus_ids_path = 'cs336_basics/checkpoint/corpus_ids'
 validation_corpus_ids_path = 'cs336_basics/checkpoint/validation_corpus_ids'
-checkpoint_dir = 'cs336_basics/checkpoint/'
+checkpoint_dir = 'cs336_basics/checkpoint/' # must end with '/'
